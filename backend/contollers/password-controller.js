dedbb7253db6b3a8715d1181a4ce644a6ed0122e
Anujeet Swain <anujeet98@gmail.com>
2023-12-14 21:19:40 +0530

    SMTP client changed

--- backend/contollers/password-controller.js
+++ backend/contollers/password-controller.js
@@ -1,37 +1,35 @@
-const Sib = require('sib-api-v3-sdk');
+const mailjet = require('node-mailjet').Client.apiConnect(
+    process.env.MJ_APIKEY_PUBLIC,
+    process.env.MJ_APIKEY_PRIVATE
+  )
 const User = require('../models/user-model');
 
-const client = Sib.ApiClient.instance;
-const apiKey = client.authentications['api-key'];
-apiKey.apiKey = process.env.SIB_API_KEY;
 
 exports.getResetPassword = async(req, res, next) => {
     try{
         const email = req.params.email;
         const user = await User.findOne({where: {email: email}});
 
-        if(user){
-            const tranEmailApi = new Sib.TransactionalEmailsApi();
-    
-            const sender = {email: process.env.SIB_SENDER_EMAIL, name: process.env.SIB_SENDER_NAME};
-            const receiver = {email: email};
-        
-            await tranEmailApi.sendTransacEmail({
-                sender,
-                to: receiver,
-                subject: 'PASSWORD-RESET: Expense Tracker',
-                htmlContent: `
+        if(user){            
+            const request = await mailjet.post('send').request({
+                FromEmail: process.env.SIB_SENDER_EMAIL,
+                FromName: process.env.SIB_SENDER_NAME,
+                Subject: 'PASSWORD-RESET: Expense Tracker',
+                'Text-part':
+                  'Dear passenger, welcome to Mailjet! May the delivery force be with you!',
+                'Html-part': `
                     <h1>Expense Tracker - Password Reset</h1><br>
                     <br>
                     <h3>Dear ${user.username},</h3><br>
                     <br>
-                    <p>Please use the following link to <a href="${process.env.APP_URL}">reset</a> the password.</p><br>
+                    <p>Please use the following link to </p> <a href="http://localhost:9000/password/resetpassword/${user.id}">reset</a> <p> your password.</p><br>
                     <br><br>
                     <h5>Thank You<h5><br>
-                `
-            });
-        
-            res.status(200).json({message: `We have sent the password reset link to ${email}`});
+                `,
+                Recipients: [{ Email: email }],
+            });  
+            if(request)   
+                res.status(200).json({message: `We have sent the password reset link to ${email}. Please check your spam for the reset email`});   
         }
         else{
             res.status(404).json({error: `${email} is not a registered email`});
