60357b3f9599c1978e081adbd99278a74a992697
Anujeet Swain <anujeet98@gmail.com>
2024-02-01 15:43:12 +0530

    mongodb-mongoose connection initialized

--- app.js
+++ app.js
@@ -2,27 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');   
 const bodyParser = require('body-parser');
-const {Sequelize} = require('sequelize');
-const helmet = require('helmet');
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
-const https = require('https');
+const mongoose = require('mongoose');
+// const helmet = require('helmet');
+// const https = require('https');
 // const compression = require('compression');
 
+// const User = require('./models/user-model');
+// const Order = require('./models/order-model');
+// const Expense = require('./models/expense-model');
+// const ForgetPassword = require('./models/forget-password');
+// const Download = require('./models/download-model');
 
-const sequelize = require('./util/db');
-const User = require('./models/user-model');
-const Order = require('./models/order-model');
-const Expense = require('./models/expense-model');
-const ForgetPassword = require('./models/forget-password');
-const Download = require('./models/download-model');
-
-const expenseRoutes = require('./routes/expense-route');
-const userRoutes = require('./routes/user-route');
-const purchaseRoutes = require('./routes/purchase-route');
-const premiumRoutes = require('./routes/premium-route');
-const passwordRoutes = require('./routes/password-route');
+// const expenseRoutes = require('./routes/expense-route');
+// const userRoutes = require('./routes/user-route');
+// const purchaseRoutes = require('./routes/purchase-route');
+// const premiumRoutes = require('./routes/premium-route');
+// const passwordRoutes = require('./routes/password-route');
 
 //---------------------------------------------------------------
 
@@ -43,30 +41,30 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 //--------------------------------------------------------------
 
-app.use('/user', userRoutes);
-app.use('/expense', expenseRoutes);
-app.use('/purchase', purchaseRoutes);
-app.use('/premium', premiumRoutes);
-app.use('/password', passwordRoutes);
+// app.use('/user', userRoutes);
+// app.use('/expense', expenseRoutes);
+// app.use('/purchase', purchaseRoutes);
+// app.use('/premium', premiumRoutes);
+// app.use('/password', passwordRoutes);
 
-app.use((req,res) => {
-    res.sendFile(path.join(__dirname, `/views/${req.url}`));
-})
+// app.use((req,res) => {
+//     res.sendFile(path.join(__dirname, `/views/${req.url}`));
+// })
 
 
 //-------------------------------------------------------------
 
-User.hasMany(Expense);
-Expense.belongsTo(User);
+// User.hasMany(Expense);
+// Expense.belongsTo(User);
 
-User.hasMany(Order);
-Order.belongsTo(User);
+// User.hasMany(Order);
+// Order.belongsTo(User);
 
-User.hasMany(ForgetPassword);
-ForgetPassword.belongsTo(User);
+// User.hasMany(ForgetPassword);
+// ForgetPassword.belongsTo(User);
 
-User.hasMany(Download);
-Download.belongsTo(User);
+// User.hasMany(Download);
+// Download.belongsTo(User);
 
 
 //-------------------------------------------------------------
@@ -74,23 +72,16 @@ Download.belongsTo(User);
 // const privateKey = fs.readFileSync('server.key');
 // const certificate = fs.readFileSync('server.cert');
 
-const serverSync = async() => {
+const serverInit = async function (){
     try{
-        // await sequelize.sync({force:true});
-        await sequelize.sync
+        const con = await mongoose.connect('mongodb+srv://anujeet98:Hk42VvtrhbulVyXP@cluster0.ijrpm3b.mongodb.net/expenseTracker?retryWrites=true&w=majority');
+        // console.log(con);
         app.listen(process.env.APP_PORT || 3000);
-
-        // https
-        //     .createServer({key: privateKey, cert: certificate},app)
-        //     .listen(process.env.APP_PORT || 3000);
-
         console.log(`Server is running on PORT: ${process.env.APP_PORT}`);
     }
     catch(err){
         console.error(err);
     }
-}
-
-serverSync();
-
+};
+serverInit();
 
